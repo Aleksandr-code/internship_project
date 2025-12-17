ba8ad76643232fca967cf7178318b68a22a02284
Aleksandr-code <70668106+Aleksandr-code@users.noreply.github.com>
2025-12-17 20:46:05 +0500

    installed bundles and vue

---
+++ app/assets/vue/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+    import { ref } from 'vue'
+
+    const test = ref('text')
+</script>
+
+<template>
+    <div>Hello {{test}}</div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ app/assets/vue/vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import symfonyPlugin from "vite-plugin-symfony";
+import vue from '@vitejs/plugin-vue'
+
+/* if you're using React */
+// import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+    plugins: [
+        /* react(), // if you're using React */
+        vue(),
+        symfonyPlugin(),
+    ],
+    build: {
+        outDir:'../../public/build',
+        rollupOptions: {
+            input: {
+                app: "app.js"
+            },
+        }
+    },
+});
